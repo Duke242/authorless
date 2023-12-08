11cca8be8e5116dca7d53a3618386d8548ab759c
Duke 0 <innominate3301@proton.me>
2023-12-07 22:49:16 -0500

    working like button

---
+++ libs/infiniteBrowserPosts.js
@@ -0,0 +1,19 @@
+import { createSupabaseBrowserClient } from "./createSupabaseBrowserClient"
+export const POSTS_PER_LOAD = 3
+export const infiniteBrowserPosts = async ({ pageParam = 1 }) => {
+  const supabase = createSupabaseBrowserClient()
+  let { data: posts, error } = await supabase
+    .from("posts")
+    .select(
+      `
+        *,
+        likes (post_id,user_id),
+        bookmarks (post_id,user_id)
+      `
+    )
+    .order("created_at", { ascending: false })
+    .range((pageParam - 1) * POSTS_PER_LOAD, pageParam * POSTS_PER_LOAD - 1)
+  if (error) throw new Error(error)
+
+  return posts
+}
